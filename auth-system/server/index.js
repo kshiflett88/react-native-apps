9479d9e0bb4a89c98eed47bbba19d6b8fe09b452
kshiflett88 <kodishiflett@gmail.com>
2021-01-11 14:08:03 -0800

    SExpress routes and user auth

---
+++ auth-system/server/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(express.json());
+
+const authRoutes = require('./routes/auth');
+const verifyToken = require('./routes/verifyToken');
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the auth system');
+})
+
+app.use('/api/users', authRoutes)
+
+app.get('/api/user/profile', verifyToken, (req, res) => {
+  res.send('This is the user profile')
+})
+
+mongoose.connect('mongodb+srv://kodi_kodes_auth:b12S06ErlM0JXQiy@cluster0.63ibp.mongodb.net/auth_system?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => {
+    app.listen(3000, () => console.log('Server is running'));
+  })
+  .catch(err => console.log(err))
